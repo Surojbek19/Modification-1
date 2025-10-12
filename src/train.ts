c5f12bc6bcad4c808ed254b059860053280e3bb2
Sam <surojbeksv@gmail.com>
2025-10-12 21:17:09 +0900

    feat: TASK H is done

--- src/train.ts
+++ src/train.ts
@@ -3,8 +3,10 @@
 //faqat positive raqamlarni olib string holatida return qilsin 
 //MASALAN: getPositive([1, -4, 2]) return qiladi "12"
 
-function getPositive(array) {
-    let positiveNum = array.filter((num) => {
-        return num >= 0;
-    })
+function getPositive(array: number[]) {
+    let positiveNum = array.filter(num => num >= 0).join("");
+    return positiveNum;
+    
 }
+
+console.log(getPositive([1, -4, 2]));
