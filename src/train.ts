cafe6175449ac92ed4f7890f42de07a15f182db1
Sam <surojbeksv@gmail.com>
2025-10-12 20:30:49 +0900

    faet: Connect mongoDB via mongoose

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,10 @@
-console.log("hello world")
+//TASK H:
+// Sjunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib,
+//faqat positive raqamlarni olib string holatida return qilsin 
+//MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+
+function getPositive(array) {
+    let positiveNum = array.filter((num) => {
+        return num >= 0;
+    })
+}
