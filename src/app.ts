328d8d2d06c8eb2e4a33115b17ec5f0591c48960
Sam <surojbeksv@gmail.com>
2025-10-13 14:15:09 +0900

    feat: create routing and member controlers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import router from "./router"; 
 
 //** 1-ENTRENCE **/
 const app = express();
@@ -15,6 +16,9 @@ app.use(express.json());
 //** 3-VIEWS **/
 app.set(`views`, path.join(__dirname, `views`));
 app.set(`views enjine`, `ejs`);
+
 //** 4-ROUTERS **/
+app.use(`/`, router); //Middleware Design Pattern
+
 
 export default app; //  = module.exports = app;
