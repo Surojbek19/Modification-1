5cb652662d4e32f7174e5bb8f7286d79b79abc19
Sam <surojbeksv@gmail.com>
2025-10-14 15:42:53 +0900

    feat: develop member schema model

---
+++ src/libs/enums/member.enum.ts
@@ -0,0 +1,10 @@
+export enum Membertype {
+    USER = "USER",
+    RESTAURANT = "RESTAURANT",
+}
+
+export enum MemberStatus {
+    ACTIVE = "ACTIVE",
+    BLOCK = "BLOCK",
+    DELETED = "DELETED",
+}
