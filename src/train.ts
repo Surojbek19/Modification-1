9fbd3219c9f918087b6cfd0aa40e879105beb1b2
Sam <surojbeksv@gmail.com>
2025-10-15 03:49:16 +0900

    feat: TASK H2 is done

--- src/train.ts
+++ src/train.ts
@@ -3,10 +3,26 @@
 //faqat positive raqamlarni olib string holatida return qilsin 
 //MASALAN: getPositive([1, -4, 2]) return qiladi "12"
 
-function getPositive(array: number[]) {
-    let positiveNum = array.filter(num => num >= 0).join("");
-    return positiveNum;
+// function getPositive(array: number[]) {
+//     let positiveNum = array.filter(num => num >= 0).join("");
+//     return positiveNum;
     
+// }
+
+// console.log(getPositive([1, -4, 2]));
+
+
+
+// TASK H2: 
+
+//Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+
+//MASALAN: getDigits("m14i1t") return qiladi "141"
+
+
+function getDigits(word: string) {
+    let digits = word.split("").filter(item => !isNaN(Number(item))).join("");
+    return digits;
 }
 
-console.log(getPositive([1, -4, 2]));
+console.log(getDigits("m14i1t")); 
