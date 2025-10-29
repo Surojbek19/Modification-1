e2d06012726d2b08739e0057d9ad4ffeb39d6f25
Sam <surojbeksv@gmail.com>
2025-10-29 13:38:21 +0900

    feat: createNewProduct business logic by restaurant member

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from "express";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
-import ProductService from "../schema/Product.model";
+import ProductService from "../models/Product.service";
+import { ProductInput } from "../libs/types/product";
+import { AdminRequest } from "../libs/types/member";
 
 const productService = new ProductService();
 
 const productController: T = {};
+
+/**=>SPA<=**/
+
+/**=>SSR<=**/
 productController.getAllProducts = async(req: Request, res: Response) => {
     try{
         console.log("getAllProducts")
@@ -17,21 +23,36 @@ productController.getAllProducts = async(req: Request, res: Response) => {
     }
 };
 
-productController.createNewProduct = async(req: Request, res: Response) => {
+productController.createNewProduct = async(req: AdminRequest, res: Response) => {
     try{
         console.log("createNewProduct")
-        res.send("Done!")
-      
+        if(!req.files?.length)
+            throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
+        
+        const data: ProductInput = req.body;
+        data.productImage =req.files?.map(ele => {
+            return ele.path.replace(/\\/g, "/");
+        });
+
+        await productService.createNewProduct(data);
+        res.send(
+            `<script>alert("Sucessful creation!"): window.location.replace('admin/product/all')</script>`
+        )
+        
+
     } catch(err) {
         console.log("ERROR, createNewProduct", err)
-       if(err instanceof Errors) res.status(err.code).json(err);
-       else res.status(Errors.standart.code).json(Errors.standart)
+        const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+         res.send(
+            `<script>alert("${message}"): window.location.replace('admin/product/all')</script>`
+        )
     }
 };
 
 productController.updateChosenProduct = async(req: Request, res: Response) => {
     try{
-        console.log("updateChosenProduct")
+        console.log("updateChosenProduct");
+
       
     } catch(err) {
         console.log("ERROR, updateChosenProduct", err)
