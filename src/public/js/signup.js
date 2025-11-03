18543e2167a1214177d4fdd73377eb72960ee821
Sam <surojbeksv@gmail.com>
2025-11-03 16:19:49 +0900

    feat: comletion of admin signup process

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,7 +1,30 @@
 console.log("Signup frontend javascript file");
 
 
- $(function() { });
+ $(function() {
+    const fileTarget = $(".file-box .upload-hidden");
+    let filename;
+
+    fileTarget.on("change", function() {
+        if(window.FileReader) {
+            const uploadFile = $(this)[0].files[0];
+            const fileType = uploadFile["type"];
+            const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+            if(!validImageType.includes(fileType)) {
+                alert("Please insert only jpeg, jpg, and png!");
+            } else {
+                if(uploadFile) {
+                    console.log(URL.createObjectURL(uploadFile));
+                    $(".upload-img-frame")
+                    .attr("src", URL.createObjectURL(uploadFile))
+                    .addClass("seccess");
+                }
+                filename = $(this)[0].files[0].name;
+            }
+            $(this).siblings(".upload-name").val(filename)
+        }
+    });
+ });
 
 function validateSignupForm() {
     const memberNick = $(".member-nick").val();
@@ -23,5 +46,11 @@ function validateSignupForm() {
         alert("Password differs, please ckeck!");
          return false;
     }
-   
+    const memberImage = $(".member-image").get(0).files[0].name
+    ? $(".member-image").get(0).files[0].name
+    : null;
+    if(!memberImage) {
+        alert("Please insert resraurant image!");
+        return false;
+    }
     }
