5818d2a975e4dc98fb7990f00290bd143305b678
Sam <surojbeksv@gmail.com>
2025-11-24 13:15:51 +0900

    feat: develop getTopUsers rest Api

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -71,6 +71,19 @@ class MemberService {
         if(!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
 
         return result;
+    };
+
+    public async getTopUsers(): Promise<Member[]> {
+        const result = await this.memberModel.find({ 
+            memberStatus: MemberStatus.ACTIVE,
+            memberPoints: { $gte: 1 }, // gte => greater than 
+         }) 
+        .sort({ memberPoints: -1 })
+        .limit(4)
+        .exec();
+
+        if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+        return result;
     }
 
     /**=>SSR<=**/
