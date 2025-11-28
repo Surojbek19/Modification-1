ccdb7b4578a5118a9ed13c2e8ac8512b39d94f82
Sam <surojbeksv@gmail.com>
2025-11-28 15:04:01 +0900

    feat: develop getMyOrders rest Api

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -3,21 +3,44 @@ import { T } from "../libs/types/common";
 import { Response } from "express";
 import Errors, { HttpCode } from "../libs/Errors";
 import OrderService from "../models/Order.service";
+import { OrderInquiry } from "../libs/types/order";
+import { OrderStatus } from "../libs/enums/order.enum";
 
 const orderService = new OrderService();
 
 const orderController: T = {};
 orderController.createOrder = async (req: ExtendedRequest, res: Response) => {
-try{
-    console.log("createOrder")
-    const result = await orderService.createOrder(req.member, req.body);
+    try{
+        console.log("createOrder")
+        const result = await orderService.createOrder(req.member, req.body);
 
-    res.status(HttpCode.CREATED).json(result)
-} catch(err) {
-    console.log("ERROR, createOrder", err)
-    if(err instanceof Errors) res.status(err.code).json(err);
-    else res.status(Errors.standart.code).json(Errors.standart)
-}
+        res.status(HttpCode.CREATED).json(result)
+    } catch(err) {
+        console.log("ERROR, createOrder", err)
+        if(err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standart.code).json(Errors.standart)
+    }
+};
+
+orderController.getMyOrders = async (req: ExtendedRequest, res: Response) => {
+    try{
+        console.log("getMyOrders")
+        const { page, limit, orderStatus } = req.query;
+        const inquiry: OrderInquiry = {
+            page: Number(page),
+            limit: Number(limit),
+            orderStatus: orderStatus as OrderStatus,
+        };
+
+        console.log("inquiry:", inquiry)
+        const result = await orderService.getMyOrders(req.member, inquiry);
+
+        res.status(HttpCode.CREATED).json(result)
+    } catch(err) {
+        console.log("ERROR, getMyOrders", err)
+        if(err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standart.code).json(Errors.standart)
+    }
 }
 
 export default orderController;
