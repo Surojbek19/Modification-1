918d28d8cad1e411e860a3ca1ba2a6ea04788869
Sam <surojbeksv@gmail.com>
2025-12-17 01:36:06 +0900

    fix: correct updateOrder business logic

--- src/models/Order.service.ts
+++ src/models/Order.service.ts
@@ -64,7 +64,7 @@ class OrderService {
         const result = await this.orderModel
         .aggregate([
             {$match: matches},
-            {$sort: { updaateAt: -1 }},
+            {$sort: { updatedAt: -1 }},
             {$skip: (inquiry.page - 1) * inquiry.limit },
             {$limit: inquiry.limit},
             {
